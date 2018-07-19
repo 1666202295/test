b9d84daa87e1ebf4a265f40a873ba3eee9335441
caohuijin <caohuijin@wzdai.com>
2018-07-19 14:52:33 +0800

    第一段python代码 打印hello world

---
+++ demo/python_if.py
@@ -0,0 +1,10 @@
+# python 中的条件控制语句 if
+
+# python 为弱类型语言,声明参数不需要声明类型
+# python 通过换行控制语句的结束,所以没有结束符
+num = 1
+# python 通过四个空格表示代码结构 通过冒号表示一段代码块
+if num == 1:
+    print("num = 1")
+else:
+    print("num != 1")
